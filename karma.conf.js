6f21bc6fc4696d98ada34d3058739ece13cf583a
Chris Tucker <chris.tucker@service-now.com>
2013-09-06 11:13:27 -0700

    Node, bower, test, and angular configuration

---
+++ karma.conf.js
@@ -0,0 +1,53 @@
+module.exports = function(config) {
+	config.set({
+
+		basePath: '',
+
+		frameworks: ["jasmine"],
+
+		preprocessors: {
+			'tpl/*.html': ['ng-html2js']
+		},
+
+		files: [
+			'bower_components/angular/angular.js',
+			'bower_components/angular-mocks/angular-mocks.js',
+			'js/**/*.js',
+			'test/unit/**/*.js',
+			'tpl/*.html'
+		],
+
+		autoWatch: true,
+
+		browsers: ['Chrome'],
+
+		reporters: ['progress', 'osx'],
+
+//		logLevel: config.LOG_ALL,
+
+		plugins: [
+			'karma-jasmine',
+			'karma-chrome-launcher',
+			'karma-osx-reporter',
+			'karma-ng-html2js-preprocessor'
+		],
+
+		ngHtml2JsPreprocessor: {
+			// strip this from the file path
+			//stripPrefix: 'public/',
+			// prepend this to the
+			//prependPrefix: 'served/',
+
+			// or define a custom transform function
+			//cacheIdFromPath: function(filepath) {
+			//	return cacheId;
+			//},
+
+			// setting this option will create only a single module that contains templates
+			// from all the files, so you can load them all with module('foo')
+			moduleName: 'tpls'
+		}
+
+	});
+};
+
---
+++ test/unit/canary.js
@@ -0,0 +1,10 @@
+/* global describe, it, expect */
+'use strict';
+
+describe('canary', function() {
+
+	it('should not squawk', function() {
+		expect(true).toBe(true);
+	});
+
+});
