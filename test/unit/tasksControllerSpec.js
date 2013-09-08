b3565d3f4862a04b1bd3c3e915cc90457df017fb
Chris Tucker <chris.tucker@service-now.com>
2013-09-07 19:39:02 -0700

    Goal 3 Part 1: Add a test to verify behavior of adding multiple entries

--- test/unit/tasksControllerSpec.js
+++ test/unit/tasksControllerSpec.js
@@ -2,23 +2,39 @@
 'use strict';
 
 describe('TasksController', function() {
+	var scope, tasksCtrl;
 
 	// Pull in the "tasks" module to get our app configuration
 	beforeEach(module('tasks'));
 
-	it('records a new entry in the task list model', inject(function($rootScope, $controller) {
-		var scope, tasksCtrl;
-
+	beforeEach(inject(function($rootScope, $controller) {
 		// Create a controller, passing in a newly constructed scope
 		scope = $rootScope.$new();
 		tasksCtrl = $controller('TasksController', { $scope : scope} );
-
-		// Configure the new entry we're going to add, and add it
-		scope.newTask = { title : "New entry title" };
-		scope.addEntry();
-
-		// Verify the behavior: the entries list should now have a first element with the right title
-		expect(scope.taskList.entries[0]).toEqual({title : 'New entry title' });
 	}));
 
+	it('records a new entry in the task list model when an entry is added', function() {
+		// Configure the new entry we're going to add, and add it
+		addNewTask('New entry title');
+
+		// Verify the behavior: the entries list should now have a first element with the right title
+		expect(scope.taskList.entries[0]).toEqual(taskWithTitle('New entry title'));
+	});
+
+	it('adds new entries to the end of the list when there are repeated addEntry() calls', function() {
+		addNewTask("task 1");
+		addNewTask("task 2");
+
+		expect(scope.taskList.entries).toEqual([taskWithTitle('task 1'), taskWithTitle('task 2')]);
+	});
+
+	function taskWithTitle(taskTitle) {
+		return { title: taskTitle };
+	}
+
+	function addNewTask(newTitle) {
+		scope.newTask = { title: newTitle };
+		scope.addEntry();
+		return scope.newTask;
+	}
 });
