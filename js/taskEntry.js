57488e95899f9cfea31828ca19d3c1b61c3f0b98
Chris Tucker <chris.tucker@service-now.com>
2013-09-11 23:17:04 -0700

    Goal 11 Part 1: Pull out the task entry as a directive/template

---
+++ js/taskEntry.js
@@ -0,0 +1,11 @@
+/*global gTasks */
+'use strict';
+
+gTasks.directive('taskEntry', function() {
+
+	return {
+		restrict: 'E',
+		templateUrl : 'tpl/taskEntry.html'
+	};
+
+});
--- test/unit/taskEntrySpec.js
+++ test/unit/taskEntrySpec.js
@@ -14,5 +14,12 @@ describe('task entry directive', function() {
 		scope.$digest();
 	}));
 
+	it('includes a view div', function() {
+		expect(element.find('div').hasClass('view')).toBeTruthy();
+	});
+
+	it('includes an edit form', function() {
+		expect(element.find('form').find('input')[0]).toBeDefined();
+	});
 
 });
