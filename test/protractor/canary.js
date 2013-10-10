02a4ffb3397ba4e3d233b058b718c598efe828d6
Chris Tucker <chris.tucker@service-now.com>
2013-10-10 15:13:03 -0700

    Failing test for Angular on the page
    tag: start-here

---
+++ test/protractor/canary.js
@@ -0,0 +1,17 @@
+(function() {
+	'use strict';
+
+	describe('angularjs homepage', function() {
+		var ptor;
+
+		beforeEach(function() {
+			ptor = protractor.getInstance();
+		});
+
+		it('should be able to load the index page', function() {
+			ptor.get('/');
+		});
+	});
+
+
+})();
