331274bc3fe1b7c9dfb25c04efd3a6f44032a29f
Chris Tucker <chris.tucker@service-now.com>
2013-10-18 11:11:03 -0700

    Passing code for adding a single task to the end of the list
    tag: task1-step4

--- js/tasksController.js
+++ js/tasksController.js
@@ -2,6 +2,14 @@
 	"use strict";
 
 	angular.module('tasks').controller('TasksController', function($scope) {
+		$scope.newTask = {};
 
+		$scope.taskList = {
+			tasks: []
+		};
+
+		$scope.addTask = function() {
+			$scope.taskList.tasks.push({title : $scope.newTask.title });
+		};
 	});
 })();
