5eaa3140fd77cacfc04526df28ac88f0ccf8c21a
Chris Tucker <chris.tucker@service-now.com>
2013-10-21 15:58:41 -0700

    Passing controller spec for adding multiple tasks
    tag: task1-step6

--- test/unit/tasksControllerSpec.js
+++ test/unit/tasksControllerSpec.js
@@ -2,7 +2,6 @@
 	'use strict';
 
 	describe('TasksController', function() {
-
 		var scope;
 
 		// Pull in the "tasks" module to get our app configuration
@@ -16,12 +15,29 @@
 
 		it('records a new entry in the task list model', function() {
 			// Configure the new entry we're going to add, and add it
-			scope.newTask = { title : "New entry title" };
-			scope.addTask();
+			addNewTask("New entry title");
 
 			// Verify the behavior: the entries list should now have a first element with the right title
-			expect(scope.taskList.tasks[0]).toEqual({title : 'New entry title' });
+			expect(scope.taskList.tasks[0]).toEqual(taskWithTitle('New entry title'));
 		});
+
+		it('adds new entries to the end of the list when there are repeated addEntry() calls', function() {
+			addNewTask("task 1");
+			addNewTask("task 2");
+
+			expect(scope.taskList.tasks).toEqual([taskWithTitle('task 1'), taskWithTitle('task 2')]);
+		});
+
+
+		function taskWithTitle(taskTitle) {
+			return { title: taskTitle };
+		}
+
+		function addNewTask(newTitle) {
+			scope.newTask = { title: newTitle };
+			scope.addTask();
+			return scope.newTask;
+		}
 
 	});
 })();
