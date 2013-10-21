47e0b67686ed9d2f97289870834f74df610326c6
Chris Tucker <chris.tucker@service-now.com>
2013-10-21 15:54:17 -0700

    Failing e2e for adding multiple tasks
    tag: task1-step5

--- test/protractor/todoListSpec.js
+++ test/protractor/todoListSpec.js
@@ -9,21 +9,46 @@
 			ptor = protractor.getInstance();
 		});
 
+		function getItems() {
+			return ptor.findElements(protractor.By.css('#todo-list > li'));
+		}
+
 		it('should add a new item to the list', function() {
 			ptor.get('/');
 
 			var taskInput = ptor.findElement(protractor.By.css('#new-todo'));
-			var item = ptor.findElement(protractor.By.css('#todo-list > li:last-child'));
 
 			// Type something in, and make sure it doesn't immediately reflect
 			taskInput.sendKeys("new todo item");
-			expect(item.getText()).toNotEqual('new todo item');
+			getItems().then(function(items) {
+				expect(items.length).toBe(0);
+			});
 
 			// Submit the input and check that the result is now updated
 			taskInput.submit();
-			expect(item.getText()).toEqual("new todo item");
+			getItems().then(function(items) {
+				expect(items[0].getText()).toEqual("new todo item");
+			});
 		});
 
+		it('should add multiple items to the list in order', function() {
+			ptor.get('/');
+
+			var taskInput = ptor.findElement(protractor.By.css('#new-todo'));
+
+			taskInput.sendKeys("Item 1");
+			taskInput.submit();
+
+			taskInput.clear();
+			taskInput.sendKeys("Item 2");
+			taskInput.submit();
+
+			getItems().then(function(items) {
+				expect(items[0].getText()).toEqual('Item 1');
+				expect(items[1].getText()).toEqual('Item 2');
+				expect(items.length).toEqual(2);
+			});
+		});
 
 	});
 
