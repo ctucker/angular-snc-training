0e95d728ffe5fbe3dc5e3cb2da1f5cbf7a9b2058
Chris Tucker <chris.tucker@service-now.com>
2014-05-04 22:01:02 -0700

    Updating last entry in the list with the input task

--- src/test/plugins/com.glide.training.todomvc/e2e/todoSpec.js
+++ src/test/plugins/com.glide.training.todomvc/e2e/todoSpec.js
@@ -8,10 +8,23 @@ var TodoListPage = function() {
 	this.addTodo = function(todoTitle) {
 		this.todoInput.sendKeys(todoTitle);
 	};
+
+	this.getLastEntry = function() {
+		return element(by.css('#todo-list li:last-child'));
+	}
 };
 
 describe('todo list homepage', function() {
 	it('loads the $todo page successfully', function() {
 		new TodoListPage().get();
+	});
+
+	describe('adding a todo entry', function() {
+		it('adds a new entry to the end of the list', function() {
+			var todoListPage = new TodoListPage();
+			todoListPage.get();
+			todoListPage.addTodo("My new todo entry");
+			expect(todoListPage.getLastEntry().getText()).toEqual("My new todo entry");
+		});
 	});
 });
