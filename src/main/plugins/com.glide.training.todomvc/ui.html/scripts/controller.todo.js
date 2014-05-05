9a2b20f3d9ab0d8663c219f6019abd842e2063cc
Chris Tucker <chris.tucker@service-now.com>
2014-05-05 11:06:01 -0700

    addTask method addition

--- src/main/plugins/com.glide.training.todomvc/ui.html/scripts/controller.todo.js
+++ src/main/plugins/com.glide.training.todomvc/ui.html/scripts/controller.todo.js
@@ -2,4 +2,9 @@ angular.module('todo').controller('Todo', function($scope) {
 	"use strict";
 
 	$scope.taskList = [];
+	$scope.taskInput = '';
+
+	$scope.addTask = function() {
+		$scope.taskList.push($scope.taskInput);
+	}
 });
--- src/test/plugins/com.glide.training.todomvc/unit/test.controller.todo.js
+++ src/test/plugins/com.glide.training.todomvc/unit/test.controller.todo.js
@@ -3,13 +3,25 @@ describe('todo controller', function() {
 
 	beforeEach(module('todo'));
 
+	var scope;
+
+	beforeEach(inject(function($rootScope, $controller) {
+		scope = $rootScope.$new();
+		$controller('Todo', { $scope: scope});
+	}));
+
+
 	describe('adding a task', function() {
 
-		it('starts with an empty task list', inject(function($rootScope, $controller) {
-			var scope = $rootScope.$new();
-			$controller('Todo', { $scope : scope} );
+		it('starts with an empty task list', function() {
 			expect(scope.taskList.length).toBe(0);
-		}));
+		});
+
+		it('adds the currently input task when addTask is called', function() {
+			scope.taskInput = 'My new task';
+			scope.addTask();
+			expect(scope.taskList[0]).toEqual('My new task');
+		});
 
 	});
 
