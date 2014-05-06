004c0f2f5574b6545b1ebd1ce4c2692e312754f6
Chris Tucker <chris.tucker@service-now.com>
2014-05-06 13:49:38 -0700

    e2e test for adding multiple tasks

--- src/test/plugins/com.glide.training.todomvc/e2e/todoSpec.js
+++ src/test/plugins/com.glide.training.todomvc/e2e/todoSpec.js
@@ -13,6 +13,10 @@ var TodoListPage = function() {
 		this.todoInput.submit();
 	};
 
+	this.getEntry = function(idx) {
+		return element.all(by.repeater('task in taskList').row(idx));
+	};
+
 	this.getLastEntry = function() {
 		return this.lastEntry;
 	}
@@ -30,6 +34,15 @@ describe('todo list homepage', function() {
 			todoListPage.get();
 			todoListPage.addTodo("My new todo");
 			expect(todoListPage.getLastEntry().getText()).toEqual("My new todo");
+		});
+
+		it('adds multiple entries to the list one after another', function() {
+			var todoListPage = new TodoListPage();
+			todoListPage.get();
+			todoListPage.addTodo("My first todo");
+			todoListPage.addTodo("My second todo");
+			expect(todoListPage.getEntry(0).getText()).toEqual("My first todo");
+			expect(todoListPage.getEntry(1).getText()).toEqual("My second todo");
 		});
 
 	});
