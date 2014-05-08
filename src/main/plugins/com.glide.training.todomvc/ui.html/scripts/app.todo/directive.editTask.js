904906caf9a470e4fa2e62002f072f4e9e2282c2
Chris Tucker <chris.tucker@service-now.com>
2014-05-08 15:15:35 -0700

    Tests for edit-task focusing

--- src/main/plugins/com.glide.training.todomvc/ui.html/scripts/app.todo/directive.editTask.js
+++ src/main/plugins/com.glide.training.todomvc/ui.html/scripts/app.todo/directive.editTask.js
@@ -7,7 +7,10 @@ angular.module('todo').directive('snEditTask', function() {
 			elem.on('dblclick', function() {
 				scope.$apply(function() {
 					var task = scope.$eval(attrs.snEditTask);
-					scope.editTask(task)
+					scope.editTask(task);
+					// You need to do your focus stuff here!
+					// Remember to yield back to the browser ($timeout) before
+					//  attempting to focus so the input can come to the foreground
 				});
 			});
 		}
--- src/test/plugins/com.glide.training.todomvc/unit/test.directive.snEditTask.js
+++ src/test/plugins/com.glide.training.todomvc/unit/test.directive.snEditTask.js
@@ -3,14 +3,22 @@ describe('sn-edit-task', function() {
 
 	beforeEach(module('todo'))
 
-	var scope, element, task;
+	var scope, element, task, editInput;
 
 	beforeEach(inject(function($compile, $rootScope) {
 		scope = $rootScope.$new();
 		scope.editTask = jasmine.createSpy('editTask');
 		task = 'THIS WOULD BE A TASK';
 		scope.task = task;
-		element = angular.element('<div sn-edit-task="task">');
+		element = angular.element(
+			'<div sn-edit-task="task">' +
+				'<input class="not-edit">' +
+				'<input class="edit">' +
+			'</div>'
+		);
+
+		editInput = element[0].querySelector('input.edit');
+		spyOn(editInput, 'focus');
 
 		$compile(element)(scope);
 		scope.$digest();
@@ -20,5 +28,10 @@ describe('sn-edit-task', function() {
 		element.triggerHandler('dblclick');
 		expect(scope.editTask).toHaveBeenCalledWith(task);
 	});
+
+	it('sets focus on the edit input on a double click event', function() {
+		element.triggerHandler('dblclick');
+		expect(editInput.focus).toHaveBeenCalled();
+	})
 
 });
