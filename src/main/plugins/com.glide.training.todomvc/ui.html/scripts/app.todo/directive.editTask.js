438c358134b09d109ebbf1ccf9afce8f6a8fecec
Chris Tucker <chris.tucker@service-now.com>
2014-05-08 17:00:25 -0700

    Isolating scope in a weird and wacky (but not wonderful) way

--- src/main/plugins/com.glide.training.todomvc/ui.html/scripts/app.todo/directive.editTask.js
+++ src/main/plugins/com.glide.training.todomvc/ui.html/scripts/app.todo/directive.editTask.js
@@ -3,11 +3,15 @@ angular.module('todo').directive('snEditTask', function($timeout) {
 
 	return {
 		restrict : 'A',
+		scope : {
+			startEditing : '&taskEditor'
+		},
 		link : function(scope, elem, attrs) {
 			elem.on('dblclick', function() {
 				scope.$apply(function() {
 					var task = scope.$eval(attrs.snEditTask);
-					scope.editTask(task);
+					console.log(scope.startEditing);
+					scope.startEditing();
 					$timeout(function() {
 						elem[0].querySelector('input.edit').focus();
 					});
--- src/main/plugins/com.glide.training.todomvc/ui.html/scripts/app.todo/directive.todoList.js
+++ src/main/plugins/com.glide.training.todomvc/ui.html/scripts/app.todo/directive.todoList.js
@@ -4,17 +4,25 @@ angular.module('todo').directive('snTodoList', function() {
 	return {
 		restrict: 'E',
 		replace : true,
+		scope : {
+			tasks : '=listOfTasks',
+			mask : '=',
+			editing : '=taskBeingEdited',
+			editTask : '&',
+			removeTask : '&taskRemover',
+			stopEditing : '&editFinisher'
+		},
 		template : '<ul id="todo-list">' +
-			'<li ng-repeat="task in taskList | filter:statusMask"' +
-			'	ng-class="{ completed : task.isComplete, editing : task === taskBeingEdited }"' +
-			'	sn-edit-task="task">' +
+			'<li ng-repeat="task in tasks | filter:mask"' +
+			'	ng-class="{ completed : task.isComplete, editing : task === editing }"' +
+			'	sn-edit-task="task" task-editor="editTask({outermostTask : task})">' +
 			'	<div class="view">' +
 			'		<input class="toggle" type="checkbox" ng-model="task.isComplete" />' +
 			'		<label>{{ task.title }}</label>' +
-			'		<button class="destroy" ng-click="deleteTask(task)" />' +
+			'		<button class="destroy" ng-click="removeTask({outermostTask : task})" />' +
 			'	</div>' +
-			'	<form ng-submit="finishEditing()">' +
-			'		<input class="edit" ng-model="task.title" ng-blur="finishEditing()" />' +
+			'	<form ng-submit="stopEditing()">' +
+			'		<input class="edit" ng-model="task.title" ng-blur="stopEditing()" />' +
 			'	</form>' +
 			'</li>' +
 			'</ul>'
